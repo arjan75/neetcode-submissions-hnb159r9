90976104b54c39a8fbfb5f9139c41eb94f057e23
arjan75 <nagi.arjan75@gmail.com>
2026-04-17 20:10:21 -0700

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        length = len(grid)
+        breadth = len(grid[0])
+        numberOfIslands = 0
+
+        visited = set()
+
+        def markIsland(i, j):
+            if i < 0 or j < 0 or i >= length or j >= breadth or grid[i][j] != "1" or (i, j) in visited:
+                return
+            
+            visited.add((i, j))
+            markIsland(i+1, j)
+            markIsland(i-1, j)
+            markIsland(i, j+1)
+            markIsland(i, j-1)
+        
+        for i in range(length):
+            for j in range(breadth):
+                if grid[i][j] == "1":
+                    if (i, j) not in visited:
+                        markIsland(i, j)
+                        numberOfIslands += 1
+        return numberOfIslands
+
+        
+
+
+        
