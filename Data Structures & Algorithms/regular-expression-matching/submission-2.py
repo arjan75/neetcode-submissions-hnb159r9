7c96d900b6017031712089881a159f6bd8fc9383
arjan75 <nagi.arjan75@gmail.com>
2026-04-21 21:58:16 -0700

    Add: regular-expression-matching - submission-2

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False for _ in range(len(p)+1)] for _ in range(len(s)+1)]
+
+        dp[0][0] = True
+        for i in range(1, len(p)+1):
+            if p[i-1] == "*":
+                dp[0][i] = dp[0][i-2]
+        
+        for j in range(1, len(s)+1):
+            dp[j][0] = False
+        
+
+        for i in range(1, len(s)+1):
+            for j in range(1, len(p)+1):
+                if p[j-1] == "." or p[j-1] == s[i-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                
+                elif p[j-1] == "*":
+                    dp[i][j] = dp[i][j-2]
+
+                    if p[j-2] == s[i-1] or p[j-2] == ".":
+                        dp[i][j] = dp[i][j] or dp[i-1][j]
+                else:
+                    dp[i][j] = False
+        return dp[-1][-1]
+
+
+
+        
