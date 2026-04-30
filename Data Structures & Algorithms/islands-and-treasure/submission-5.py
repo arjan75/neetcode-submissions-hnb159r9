b0b868ec85532b81a17d8a84e7571834622b7f9c
arjan75 <nagi.arjan75@gmail.com>
2026-04-29 18:33:58 -0700

    Add: islands-and-treasure - submission-5

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        length = len(grid)
+        breath = len(grid[0])
+
+        queue = []
+        visited = set()
+        for i in range(length):
+            for j in range(breath):
+                if grid[i][j] == 0:
+                    queue.append([i, j])
+                    visited.add((i, j))
+        
+        directions = [[0,1], [0, -1], [1, 0], [-1, 0]]
+
+        distance = 0
+        while queue:
+            for _ in range(len(queue)):
+                cell = queue.pop(0)
+                cellX = cell[0]
+                cellY = cell[1]
+
+                if grid[cellX][cellY] == 2147483647:
+                    grid[cellX][cellY] = distance
+                
+                for direction in directions:
+                    newX = cellX + direction[0]
+                    newY = cellY + direction[1]
+
+                    if newX < 0 or newY < 0 or newX >= length or newY >= breath or grid[newX][newY] == -1 or (newX, newY) in visited:
+                        continue
+                    
+                    visited.add((newX, newY))
+                    queue.append([newX, newY])
+            distance += 1
+            
+                
