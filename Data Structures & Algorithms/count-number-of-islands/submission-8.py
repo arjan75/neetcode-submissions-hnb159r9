ead5a9b39309f7aac69ace105aabb5fa665cf924
arjan75 <nagi.arjan75@gmail.com>
2026-05-19 09:10:19 -0400

    Add: count-number-of-islands - submission-8

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        length = len(grid)
+        breath = len(grid[0])
+
+        def markIsland(i, j):
+            if i < 0 or j < 0 or i >= length or j >= breath or grid[i][j] != "1":
+                return
+            
+            grid[i][j] = "0"
+            markIsland(i+1, j)
+            markIsland(i-1, j)
+            markIsland(i, j+1)
+            markIsland(i, j-1)
+        
+        islands = 0
+        for i in range(length):
+            for j in range(breath):
+                if grid[i][j] == "1":
+                    markIsland(i, j)
+                    islands += 1
+        return islands
+
+
+
