38fea6c17b51f78709725a902a9307068d780ca8
arjan75 <nagi.arjan75@gmail.com>
2026-07-03 07:18:44 -0700

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,46 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courseMap = {}
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2   
+        stateMap = {}
+
+        for course in prerequisites:
+            if course[0] in courseMap:
+                courseMap[course[0]].append(course[1])
+            else:
+                courseMap[course[0]] = [course[1]]  
+            
+        stateMap = {i: UNVISITED for i in range(numCourses)}
+
+        def doesCycleExist(course):
+            if course not in courseMap:
+                return False
+            
+            stateMap[course] = VISITING
+            neighbours = courseMap[course]
+            for neighbour in neighbours:
+                if stateMap[neighbour] == 1:
+                    return True
+
+                if doesCycleExist(neighbour):
+                    return True
+
+            stateMap[course] = 2
+            return False
+        
+
+        for course in stateMap:
+            if doesCycleExist(course):
+                return False
+        return True
+
+
+
+
+            
+        
+
+
+        
