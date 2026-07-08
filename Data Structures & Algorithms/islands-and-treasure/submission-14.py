a9923e877db0da05496437f834011105f8f83f6e
arjan75 <nagi.arjan75@gmail.com>
2026-07-07 21:23:22 -0700

    Add: islands-and-treasure - submission-14

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-14.py
@@ -0,0 +1,42 @@
+from collections import deque
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        length = len(grid)
+        breath = len(grid[0])
+
+        queue = deque()
+
+        visited = set()
+        for i in range(length):
+            for j in range(breath):
+                if grid[i][j] == 0:
+                    queue.append((i, j))
+        
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        distance = 0
+        while queue:
+            for _ in range(len(queue)):
+                cell = queue.popleft()
+                cellX = cell[0]
+                cellY = cell[1]
+
+                if grid[cellX][cellY] == 2147483647:
+                    grid[cellX][cellY] = distance
+                
+                for direction in directions:
+                    newX = cellX + direction[0]
+                    newY = cellY + direction[1]
+
+                    if newX < 0 or newX >= length or newY < 0 or newY >= breath or (newX, newY) in visited or grid[newX][newY] == -1:
+                        continue
+                    
+                    queue.append((newX, newY))
+                    visited.add((newX, newY))
+            distance += 1
+         
+
+
+
+            
+
+        
